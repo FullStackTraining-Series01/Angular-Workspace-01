1ac9bf153e941ef5bb08789e6d13def3f753b008
Ajay <AjayOnline-03@yahoo.com>
2021-10-14 06:33:23 +0530

    Class 9 Angular code

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { FirstService } from './first.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirstService],
+  bootstrap: [FirstComponentComponent]  
 })
 export class AppModule { }
